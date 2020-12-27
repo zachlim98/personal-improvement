5514bc6d39c565c8ece588ae9f880a06a13ddfe5
zachlim98 <zachlim98@users.noreply.github.com>
2020-12-27 11:16:29 +0800

    Random matplotlib crash coursing

---
+++ matplotlib training/.virtual_documents/Untitled.ipynb.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+
+plt.plot([2,3,4],[3,2,5], 'bH--', label="hump")
+plt.title("My First Graphget_ipython().getoutput("")")
+plt.xlabel("Xlabel")
+plt.ylabel("Ylabel")
+
+plt.xticks([2,3,4,5])
+plt.yticks([2,3,4,5])
+plt.legend()
+
+plt.show()
+
+
+
